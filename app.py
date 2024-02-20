891940e6de6553a5bcdef4a8f919ce495c38d203
Jack Ambery <jack.ambery@gmail.com>
2024-02-20 15:13:58 -0500

    oath working

--- app.py
+++ app.py
@@ -1,75 +1,78 @@
 from dotenv import load_dotenv
 import os
-import base64
-from requests import post, get
-import json
-from flask import Flask, render_template
+from flask import Flask, redirect, session, request, render_template
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
-
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = {"grant_type": "client_credentials"}
-    result = post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result["access_token"]
-    return token
-
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
-
-def search_for_artist(token, artist_name):
-    url = "https://api.spotify.com/v1/search"
-    headers = get_auth_header(token)
-    query = f"?q={artist_name}&type=artist&limit=1"
-
-    query_url = url + query
-    result = get(query_url, headers=headers)
-    json_result = json.loads(result.content)["artists"]["items"]
-    if len(json_result) == 0:
-        print("No artists with this name exists...")
-        return None
-    return json_result[0]
-
-def get_songs_by_artist(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
-    headers = get_auth_header(token)
-    result = get(url, headers=headers)
-    json_result = json.loads(result.content)["tracks"]
-    return json_result
-
-
-token = get_token()
-result = search_for_artist(token, "Metallica")
-print(result["name"])
-artist_id = result["id"]
-songs = get_songs_by_artist(token, artist_id)
-
-for idx, song in enumerate(songs):
-    print(f"{idx + 1}. {song['name']}")
-
-
-
 # This is what connects the front end to the backend
 app = Flask(__name__)
+app.secret_key = client_secret
 
 @app.route('/')
 def index():
-    #TODO use the API to get this data from the user
-    username = "jack ambery"
-    pfp = "img path"
-    return render_template('index.html', username=username, pfp=pfp)
+    # Initialize SpotifyOAuth object within the route
+    sp_oauth = SpotifyOAuth(
+        client_id = os.getenv("CLIENT_ID"),
+        client_secret = os.getenv("CLIENT_SECRET"),
+        redirect_uri = request.base_url + 'callback',
+        scope = 'user-read-private'
+    )
+    # Get user's profile information
+    token_info = session.get('spotify_token_info')
+
+    if not token_info or sp_oauth.is_token_expired(token_info):
+        return redirect('/callback')
+
+    sp = spotipy.Spotify(auth=session['spotify_token_info']['access_token'])
+    user_profile = sp.current_user()
+    username = user_profile['display_name']
+    # Get user's profile picture if available
+    pfp = user_profile['images'][0]['url'] if user_profile['images'] else ''
+    id = user_profile['id']  
+    return render_template('index.html', username=username, pfp=pfp, id=id)
+
+@app.route('/callback')
+def callback():
+    sp_oauth = SpotifyOAuth(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        redirect_uri=request.base_url,
+        scope='user-read-private'
+    )
+    # Parse authorization response
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session['spotify_token_info'] = token_info
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# These were methods used before switching to Spotipy
+
+# def search_for_artist(token, artist_name):
+#     url = "https://api.spotify.com/v1/search"
+#     headers = get_auth_header(token)
+#     query = f"?q={artist_name}&type=artist&limit=1"
+
+#     query_url = url + query
+#     result = get(query_url, headers=headers)
+#     json_result = json.loads(result.content)["artists"]["items"]
+#     if len(json_result) == 0:
+#         print("No artists with this name exists...")
+#         return None
+#     return json_result[0]
+
+# def get_songs_by_artist(token, artist_id):
+#     url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+#     headers = get_auth_header(token)
+#     result = get(url, headers=headers)
+#     json_result = json.loads(result.content)["tracks"]
+#     return json_result
