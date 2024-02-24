6125b29f95169f3d7899900790b5650ebaf8fb4e
Jack Ambery <jack.ambery@gmail.com>
2024-02-24 16:52:13 -0500

    music samples play when albums are clicked

--- app.py
+++ app.py
@@ -50,13 +50,15 @@ def index():
     topSongs = sp.current_user_top_tracks(limit=6)
     names = []
     pics = []
+    previews = []
     for song in topSongs['items']:
         names.append(song['name'])
+        previews.append(song['preview_url'])
         album_id = song['album']['id']
         album_info = sp.album(album_id)
         cover_art_url = album_info['images'][0]['url'] if len(album_info['images']) > 0 else None
         pics.append(cover_art_url)
-    songData = zip(pics, names)
+    songData = zip(pics, names, previews)
     
     return render_template('profile.html', username=username, pfp=pfp, id=id, songData=songData)
 
