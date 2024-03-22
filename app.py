c9deb9feba7c2c54474332e956743b88083b7e1d
Jack Ambery <jack.ambery@gmail.com>
2024-03-22 14:45:05 -0400

    i think i am getting somewhere

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from flask import Flask, redirect, session, request, render_template, make_respo
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+import sqlite3
+from flask import jsonify
+
 load_dotenv()
 
 # this code is retarded because it's caching everyones user auth in the .cache file
@@ -13,6 +18,38 @@ client_secret = os.getenv("CLIENT_SECRET")
 # This is what connects the front end to the backend
 app = Flask(__name__)
 app.secret_key = client_secret
+
+# DATABASE
+db = SQLAlchemy()
+db_name = 'tunetown.db'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    
+    # Relationship with friends
+    friends = db.relationship('Friendship', foreign_keys='Friendship.user_id', backref='user', lazy=True)
+
+    # Relationship with posts
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+class Friendship(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 # Initialize SpotifyOAuth object within the route
 sp_oauth = SpotifyOAuth(
@@ -42,10 +79,26 @@ def index():
     sp = spotipy.Spotify(auth=session['spotify_token_info']['access_token'])
     user_profile = sp.current_user()
 
+    # LOAD DB
+    # try:
+    #     users = db.session.execute(db.select(User)
+    #         .order_by(User.username)).scalars()
+
+    #     username_text = '<ul>'
+    #     for user in users:
+    #         username_text += '<li>' + user.username + ', ' + str(user.id) + '</li>'
+    #     username_text += '</ul>'
+    #     return username_text
+    # except Exception as e:
+    #     # e holds description of the error
+    #     error_text = "<p>The error:<br>" + str(e) + "</p>"
+    #     hed = '<h1>Something is broken.</h1>'
+    #     return hed + error_text
+
     # user variables
     username = user_profile['display_name']
     pfp = user_profile['images'][0]['url'] if user_profile['images'] else ''
-    id = user_profile['id']  
+    id = user_profile['id']
 
     topSongs = sp.current_user_top_tracks(limit=6)
     names = []
@@ -83,6 +136,41 @@ def logout():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+# METHODS
+# Route to add a new user
+@app.route('/add_user', methods=['POST'])
+def add_user(username):
+
+    # # Check if username is provided
+    # if not username:
+    #     return jsonify({'message': 'Username is required'}), 400
+
+    # Check if the username already exists
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message': 'Username already exists'}), 400
+
+    # Create a new user object
+    new_user = User(username=username)
+
+    # Add the user to the session and commit changes to the database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User added successfully'}), 201
+
+@app.route('/add_post', methods=['POST'])
+def add_post(song_id):
+
+    # Create a new user object
+    # TODO this prob is not correct
+    new_post = Post(content = song_id, user_id = User.id)
+
+    # Add the user to the session and commit changes to the database
+    db.session.add(new_post)
+    db.session.commit()
+
+    return jsonify({'message': 'User added successfully'}), 201
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
